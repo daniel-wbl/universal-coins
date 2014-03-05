674d7d2611a4b082c2d45c9909016a627a2af3ed
WBL_b3nl <b.lange@wbl-konzept.de>
2014-03-06 00:17:51 +0100

    + more simple calls

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
+path = require('path');
 var oExpress = require('express'), oApp = oExpress();
+var nano = require('nano')('http://localhost:5984');
+
+oApp.use(oExpress.static(__dirname + path.sep + 'public'));
+
+oApp.set('views', __dirname + path.sep + 'views');
+oApp.set('view engine', 'jade');
+oApp.engine('jade', require('jade').__express);
 
 oApp.get('/', function (oRequest, oResponse) {
-	var sBody = 'Hello World';
-	oResponse.writeHead(200, {'Content-Length': Buffer.byteLength(sBody), 'Content-Type': 'text/plain'});
-	oResponse.end(sBody);
+	oResponse.render('index');
 });
+
+oApp.get(
+	'/user/:id',
+	function(oRequest, oResponse) {
+		var oCoins = nano.use('universal_coins');
+
+		oCoins.get(oRequest.params.id, function (bError, oBody) {
+			if (bError) {
+				var sBody = "Not found";
+				oResponse.writeHead(404, {'Content-Length': Buffer.byteLength(sBody), 'Content-Type': 'text/plain'});
+				oResponse.end(sBody);
+			} else {
+				var sBody = oBody.coins + '';
+
+				oResponse.writeHead(200, {'Content-Length': Buffer.byteLength(sBody), 'Content-Type': 'text/plain'});
+				oResponse.end(sBody);
+			} // else
+		});
+	}
+
+)
 
 oApp.listen(Number(process.argv[2]));
