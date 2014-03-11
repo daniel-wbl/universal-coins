5916d41fb7ccbd9cba90cbf702cb409a91dae29e
Daniel Sadlowski <d.sadlowski@wbl-konzept.de>
2014-03-11 16:50:40 +0100

    *html erstellung

--- foundation/config.rb
+++ foundation/config.rb
@@ -1,15 +1,15 @@
 # Require any additional compass plugins here.
-add_import_path "../foundation/foundation/scss"
+add_import_path "../public/foundation/foundation/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
+css_dir = "../public/css"
 sass_dir = "scss"
-images_dir = "images"
-javascripts_dir = "js"
+images_dir = "../public/img"
+javascripts_dir = "../public/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+# output_style = :compact
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
